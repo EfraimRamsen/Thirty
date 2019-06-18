88888e1c342f10d38759122cbbfd5e16e51ef8cd
EfraimRamsen <38281350+EfraimRamsen@users.noreply.github.com>
2019-06-18 22:38:31 +0200

    Add welcome text and background color

--- app/src/main/java/com/efraim/thirty/MainActivity.java
+++ app/src/main/java/com/efraim/thirty/MainActivity.java
@@ -2,8 +2,13 @@ package com.efraim.thirty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+	private ImageButton mDice1Button, mDice2Button, mDice3Button, mDice4Button, mDice5Button, mDice6Button;
+	private Button mRollButton, mConfirmButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
