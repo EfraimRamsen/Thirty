6c93f3dae79598489300bbc0a02da58298698faf
EfraimRamsen <38281350+EfraimRamsen@users.noreply.github.com>
2019-06-25 14:37:05 +0200

    Change instruction text and dice icons when using roll button & confirm button

--- app/src/main/java/com/efraim/activity/MainActivity.java
+++ app/src/main/java/com/efraim/activity/MainActivity.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.efraim.model.*;
 
 public class MainActivity extends AppCompatActivity {
 
+	private Game mGame;
+	private TextView mInstructionsTextView;
 	private ImageButton[] mDiceButtonArray;
 	private Button mRollButton;
 	private Button mConfirmButton;
-	private Game mGame;
+
+	private static final int DICE_OFF = 0;
+	private static final int DICE_STANDARD = 1;
+	private static final int DICE_LOCKED = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +27,14 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		mGame = new Game();
-
+		mInstructionsTextView = findViewById(R.id.instructions);
 		mDiceButtonArray  =
-				new ImageButton[]{findViewById(R.id.dice1_button),
-						findViewById(R.id.dice2_button),
-						findViewById(R.id.dice3_button),
-						findViewById(R.id.dice4_button),
-						findViewById(R.id.dice5_button),
-						findViewById(R.id.dice6_button),
+				new ImageButton[]{  findViewById(R.id.dice1_button),
+									findViewById(R.id.dice2_button),
+									findViewById(R.id.dice3_button),
+									findViewById(R.id.dice4_button),
+									findViewById(R.id.dice5_button),
+									findViewById(R.id.dice6_button),
 				};
 
 		mConfirmButton = findViewById(R.id.confirm_button);
@@ -38,23 +44,41 @@ public class MainActivity extends AppCompatActivity {
 		mRollButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				System.out.println(mGame.mDiceArrayToString());
-				mGame.rollAllDice(); // funkar
-				System.out.println(mGame.mDiceArrayToString());
-				updateDiceIcons(); //todo funkar inte?
+				mGame.rollAllDice();
+				updateDiceIcons();
 
 				mConfirmButton.setEnabled(true);
 				mConfirmButton.setTextColor(getResources().getColor(R.color.colorAccent));
 
 				mRollButton.setEnabled(false);
 				mRollButton.setTextColor(getResources().getColor(R.color.grey_text));
+
+				mInstructionsTextView.setText(R.string.instructions_after_roll);
 			}
 		});
+
+
 
 		mConfirmButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				for(int i = 0; i < mDiceButtonArray.length; i++){
+//					Dice dice = mGame.getDiceForButton(i);
+//					mDiceButtonArray[i].setBackgroundResource(dice.setDiceImage(dice.getDiceScore(),dice.getDiceState())
+//					);
+					Dice dice = mGame.getDiceForButton(i);
+					if(dice.getDiceState() == DICE_STANDARD){
+						dice.setDiceState(DICE_LOCKED);
+						updateDiceIcons();
+					}
+					mConfirmButton.setEnabled(false);
+					mConfirmButton.setTextColor(getResources().getColor(R.color.grey_text));
 
+					mRollButton.setEnabled(true);
+					mRollButton.setTextColor(getResources().getColor(R.color.colorAccent));
+
+					mInstructionsTextView.setText(R.string.instructions_after_confirm);
+				}
 			}
 		});
 	}
--- app/src/main/java/com/efraim/model/Game.java
+++ app/src/main/java/com/efraim/model/Game.java
@@ -4,36 +4,40 @@ import java.util.Random;
 
 public class Game {
 	private Dice[] mDiceArray;
+	private static final int DICE_OFF = 0;
+	private static final int DICE_STANDARD = 1;
+	private static final int DICE_LOCKED = 2;
 
 	public Game(){
 		mDiceArray = new Dice[] {
-		new Dice(1,1),
-		new Dice(2,1),
-		new Dice(3,1),
-		new Dice(4,1),
-		new Dice(5,1),
-		new Dice(6,1)};
+		new Dice(1,DICE_STANDARD),
+		new Dice(2,DICE_STANDARD),
+		new Dice(3,DICE_STANDARD),
+		new Dice(4,DICE_STANDARD),
+		new Dice(5,DICE_STANDARD),
+		new Dice(6,DICE_STANDARD)};
 
 //		Score score = new Score(); //todo använd den här, börjar på 0
 	}
 
 	public void rollAllDice(){
 		for (int i = 0; i < mDiceArray.length; i++){
-				mDiceArray[i] = rollADice(mDiceArray[i]);
-		}
-	}
-
-	public void rollSelectedOffDice(){
-		for (int i = 0; i < mDiceArray.length; i++){
-			if(mDiceArray[i].getDiceState() < 1){
+			if(mDiceArray[i].getDiceState() != DICE_LOCKED) {
 				mDiceArray[i] = rollADice(mDiceArray[i]);
 			}
 		}
 	}
 
+//	public void rollSelectedOffDice(){
+//		for (int i = 0; i < mDiceArray.length; i++){
+//				mDiceArray[i] = rollADice(mDiceArray[i]);
+//			}
+//		}
+//	}
+
 	public Dice rollADice(Dice dice){
 		dice.setDiceScore(randomD6());
-		dice.setDiceState(1);
+		dice.setDiceState(DICE_STANDARD);
 		return dice;
 	}
 
