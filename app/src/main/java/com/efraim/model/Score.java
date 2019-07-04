036417c922caf54e8c48da4e6944a7f6d489bdf9
Efraim <efra2977@student.su.se>
2019-07-04 09:27:39 +0200

    Comment and clean unused code in Score.java

--- app/src/main/java/com/efraim/model/Score.java
+++ app/src/main/java/com/efraim/model/Score.java
@@ -1,11 +1,22 @@
+
 package com.efraim.model;
 
 import java.util.ArrayList;
 
+/**
+ * Holds the total score of a game and lists of used dice for each round in
+ * the game to display at the score screen after the game is over.
+ * @author Efraim Ramsén
+ */
 public class Score {
 	private int mTotalScore = 0;
 	private ArrayList<ArrayList<Dice>> mDiceForEachRound = new ArrayList<>();
 
+	/**
+	 * Adds a list with dice to the list mDiceForEachRound and updates the
+	 * total score. It's supposed to add one list per finished round.
+	 * @param diceList the list of dice being added.
+	 */
 	public void addDiceList(ArrayList<Dice> diceList){
 		mDiceForEachRound.add(diceList);
 		mTotalScore = 0;
@@ -16,21 +27,5 @@ public class Score {
 				mTotalScore += d.getDiceScore();
 			}
 		}
-	}
-
-	public int getTotalScore() {
-		return mTotalScore;
-	}
-
-	public void setTotalScore(int totalScore) {
-		this.mTotalScore = totalScore;
-	}
-
-	public ArrayList<ArrayList<Dice>> getDiceForEachRound() {
-		return mDiceForEachRound;
-	}
-
-	public void setDiceForEachRound(ArrayList<Dice> mDiceListForEachRound) {
-		this.mDiceForEachRound.add(mDiceListForEachRound);
 	}
 }
